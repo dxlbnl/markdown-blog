31ebb88d09d413d1b727d06ae3c4076c84756a85
Dexter <dexter@dexterlabs.nl>
2018-07-07 18:33:42 +0200

    Should push this :S

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ import { routes } from './manifest/server.js'
 import App from './App.html'
 
 polka() // You can also use Express
-  .use(
+  .use('/blog',
     compression({ threshold: 0 }),
     sirv('assets'),
     sapper({ routes, App })
